3a32719a42a0c5ceb50f671efc549a3f052a8a24
Jamal Ali-Mohammed <jamal.ali.mohammed@gmail.com>
2017-07-18 21:35:05 -0400

    First commit.

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('sass-compile', function() {
+    gulp.src('./_build/_sass/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(autoprefixer({browsers: ['last 2 versions']}))
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('./code/html/assets/css/'))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('sync', function() {
+    browserSync.init({
+        server: { baseDir: './code/html' }
+    });
+});
+
+gulp.task('serve', ['sass-compile', 'sync'], function() {
+    gulp.watch('./_build/_sass/**/*.scss', ['sass-compile']);
+    gulp.watch('**', {cwd: './code/html'}, reload);
+});
