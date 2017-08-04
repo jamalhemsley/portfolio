65869235319468f963b5b6e4a2dc6cd7cbfe58ae
Jamal Ali-Mohammed <jamal.ali.mohammed@gmail.com>
2017-08-03 23:39:36 -0400

    Restructured folders to work properly with Github Pages.

---
+++ assets/js/core.js
@@ -0,0 +1,6 @@
+/* Title: Miscellaneous JavaScript Code for the Portfolio Site
+ * Author URI: https://jdaio.github.io
+ * Author Name: Jamal Ali-Mohammed
+ * Original Date: Thursday, August 3, 2017
+ * Updated Date: Check the Github Timestamp.
+ */
--- gulpfile.js
+++ gulpfile.js
@@ -10,17 +10,17 @@ gulp.task('sass-compile', function() {
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(autoprefixer({browsers: ['last 2 versions']}))
         .pipe(rename('style.min.css'))
-        .pipe(gulp.dest('./code/html/assets/css/'))
+        .pipe(gulp.dest('./assets/css/'))
         .pipe(reload({stream: true}));
 });
 
 gulp.task('sync', function() {
     browserSync.init({
-        server: { baseDir: './code/html' }
+        server: { baseDir: './' }
     });
 });
 
 gulp.task('serve', ['sass-compile', 'sync'], function() {
     gulp.watch('./_build/_sass/**/*.scss', ['sass-compile']);
-    gulp.watch('**', {cwd: './code/html'}, reload);
+    gulp.watch('**', {cwd: './'}, reload);
 });
