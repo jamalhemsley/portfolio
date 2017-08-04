fc94b98887841e5696e124db836e3ab66043db8b
Jamal Ali-Mohammed <jamal.ali.mohammed@gmail.com>
2017-08-03 23:44:09 -0400

    Removed unused folder.

--- code/html/assets/js/core.js
+++
@@ -1,6 +0,0 @@
-/* Title: Miscellaneous JavaScript Code for the Portfolio Site
- * Author URI: https://jdaio.github.io
- * Author Name: Jamal Ali-Mohammed
- * Original Date: Thursday, August 3, 2017
- * Updated Date: Check the Github Timestamp.
- */
