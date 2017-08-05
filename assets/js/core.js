be8a37d3260ad95e05960107568bcf56702cb3e9
Jamal Ali-Mohammed <jamal.ali.mohammed@gmail.com>
2017-08-05 14:16:30 -0400

    Updated stuff 8/5/17.

--- assets/js/core.js
+++ assets/js/core.js
@@ -4,3 +4,10 @@
  * Original Date: Thursday, August 3, 2017
  * Updated Date: Check the Github Timestamp.
  */
+
+window.addEventListener('load', function() {
+    document.querySelector('.smoothscroll').addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector('#a--footer-contact').scrollIntoView({ behavior: 'smooth' });
+    });
+});
