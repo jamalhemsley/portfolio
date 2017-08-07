8cfe7eae846a5bb22c2fd2bebd3afc5f4a1566d0
Jamal Ali-Mohammed <jamal.ali.mohammed@gmail.com>
2017-08-07 13:20:01 -0400

    Rolled back gulpfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
 gulp.task('sass-compile', function() {
-    gulp.src('./_build/_sass/**/*.scss')
+    gulp.src('_build/_sass/**/*.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(autoprefixer({browsers: ['last 2 versions']}))
         .pipe(rename('style.min.css'))
@@ -16,11 +16,11 @@ gulp.task('sass-compile', function() {
 
 gulp.task('sync', function() {
     browserSync.init({
-        server: { baseDir: './' }
+        server: { baseDir: '/' }
     });
 });
 
 gulp.task('serve', ['sass-compile', 'sync'], function() {
-    gulp.watch('./_build/_sass/**/*.scss', ['sass-compile']);
-    gulp.watch('**', {cwd: './'}, reload);
+    gulp.watch('_build/_sass/**/*.scss', ['sass-compile']);
+    gulp.watch('**', {cwd: '/'}, reload);
 });
