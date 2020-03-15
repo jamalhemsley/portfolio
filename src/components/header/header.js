505cc38b9e59d39a5e744ea8700a498eadff3132
Jamal Ali-Mohammed <9924527+jdaio@users.noreply.github.com>
2020-03-14 21:28:10 -0400

    Updated social media link titles

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -74,6 +74,7 @@ function Header({ id, isDark }) {
                             <li className={style.navItem}>
                                 <a
                                     className={style.navLink}
+                                    title={`@${site.siteMetadata.social.github} on Github`}
                                     href={`https://github.com/${site.siteMetadata.social.github}`}
                                     target="_blank"
                                     rel="noopener noreferrer"
@@ -84,7 +85,7 @@ function Header({ id, isDark }) {
                                         viewBox="0 0 24 24"
                                         xmlns="http://www.w3.org/2000/svg"
                                     >
-                                        <title>GitHub</title>
+                                        <title>GitHub Logo</title>
                                         <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
                                     </svg>
                                 </a>
@@ -92,6 +93,7 @@ function Header({ id, isDark }) {
                             <li className={style.navItem}>
                                 <a
                                     className={style.navLink}
+                                    title={`@${site.siteMetadata.social.linkedin} on LinkedIn`}
                                     href={`https://linkedin.com/in/${site.siteMetadata.social.linkedin}`}
                                     target="_blank"
                                     rel="noopener noreferrer"
@@ -102,7 +104,7 @@ function Header({ id, isDark }) {
                                         viewBox="0 0 24 24"
                                         xmlns="http://www.w3.org/2000/svg"
                                     >
-                                        <title>LinkedIn</title>
+                                        <title>LinkedIn Logo</title>
                                         <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
                                     </svg>
                                 </a>
@@ -110,6 +112,7 @@ function Header({ id, isDark }) {
                             <li className={style.navItem}>
                                 <a
                                     className={style.navLink}
+                                    title={`Send mail to ${site.siteMetadata.social.email}`}
                                     href={`mailto:${site.siteMetadata.social.email}?subject=Hello! 👋 Let's Work Together.`}
                                 >
                                     <svg
@@ -118,7 +121,7 @@ function Header({ id, isDark }) {
                                         viewBox="0 0 479.058 479.058"
                                         xmlns="http://www.w3.org/2000/svg"
                                     >
-                                        <title>Email</title>
+                                        <title>Email Icon</title>
                                         <path d="M434.146 59.882H44.912C20.146 59.882 0 80.028 0 104.794v269.47c0 24.766 20.146 44.912 44.912 44.912h389.234c24.766 0 44.912-20.146 44.912-44.912v-269.47c0-24.766-20.146-44.912-44.912-44.912zm0 29.941c2.034 0 3.969.422 5.738 1.159L239.529 264.631 39.173 90.982c1.769-.736 3.704-1.159 5.738-1.159zm0 299.411H44.912c-8.26 0-14.971-6.71-14.971-14.971V122.615l199.778 173.141c2.822 2.441 6.316 3.655 9.81 3.655s6.988-1.213 9.81-3.655l199.778-173.141v251.649c-.001 8.26-6.711 14.97-14.971 14.97z" />
                                     </svg>
                                 </a>
