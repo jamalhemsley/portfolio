2b82846e7cce5aabfed283ae9a0ab9dab940ca41
Jamal Ali-Mohammed <jamal@digitalhe.at>
2021-01-08 16:56:54 -0500

    changed critical css load point

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// Import Global CSS & Critical Components
+// Import Global CSS
 import 'styles/global.scss';
-import '../components/global/Header/Header.module.scss';
-import '../layouts/Default/DefaultLayout.module.scss';
-import '../components/common/Button/Button.module.scss';
-import '../components/common/ButtonGroup/ButtonGroup.module.scss';
 
 const App = ({ Component, pageProps }) => (
   <>
