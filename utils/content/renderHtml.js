4156e1d3187a006095ee8bd20a13296a4086bc58
Jamal Ali-Mohammed <jamal@digitalhe.at>
2021-01-11 18:10:30 -0500

    Update html serializer with smoothscrolling

--- utils/content/renderHtml.js
+++ utils/content/renderHtml.js
@@ -1,7 +1,17 @@
+import React from 'react';
+import { Elements } from 'prismic-reactjs';
 import smartquotes from 'smartquotes';
+import { linkResolver } from 'config/prismic';
+
+// Add unique key to props.
+const propsWithUniqueKey = function (props, key) {
+  return Object.assign(props || {}, { key });
+};
 
 // HTML serializer to customize Prismic RichText output.
-const renderHtml = (type, element, content, children) => {
+const renderHtml = (type, element, content, children, key) => {
+  let props = {};
+
   /* eslint-disable no-param-reassign */
   // Loop through arrays to convert quotes in strings to smart quotes.
   children.forEach((child, index) => {
@@ -21,6 +31,50 @@ const renderHtml = (type, element, content, children) => {
     }
   });
   /* eslint-enable no-param-reassign */
+
+  switch (type) {
+    case Elements.hyperlink: {
+      // Utilize `smoothscroll-polyfill` to enable smooth scrolling for Safari.
+      const smoothScroll = (e) => {
+        const target = e.currentTarget.getAttribute('href');
+
+        if (target[0] === '#') {
+          e.preventDefault();
+
+          document
+            .getElementById(target.substring(1))
+            .scrollIntoView({ behavior: 'smooth' });
+        }
+      };
+
+      const hrefAttr = {
+        href: element.data.url || linkResolver(element.data),
+      };
+
+      if (hrefAttr.href.includes('//#')) {
+        const scrollTarget = hrefAttr.href.split('#')[1];
+
+        hrefAttr.href = `#${scrollTarget}`;
+        hrefAttr.onClick = (e) => smoothScroll(e);
+      }
+
+      const targetAttr = element.data.target
+        ? { target: element.data.target }
+        : {};
+      const relAttr = element.data.target ? { rel: 'noopener' } : {};
+
+      props = {
+        ...hrefAttr,
+        ...targetAttr,
+        ...relAttr,
+      };
+
+      return React.createElement('a', propsWithUniqueKey(props, key), children);
+    }
+
+    default:
+      return null;
+  }
 };
 
 export default renderHtml;
